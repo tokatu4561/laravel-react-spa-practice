3f22846258b93c60479d624cb17397aebd4ab2e0
tokatu4561 <ryobsk4561@gmail.com>
2021-12-22 07:50:54 -0800

    post tabele ダミーデータ

---
+++ backend/database/seeders/PostsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('posts')->insert([
+            [
+                'name' => 'とかつ',
+                'content' => 'じゃがりこ',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => '牙突',
+                'content' => 'ポテト食べる',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => '勝つと',
+                'content' => 'laravel連携練習用',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]
+        ]);
+    }
+}
